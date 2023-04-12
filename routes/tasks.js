7c5f49c5a73d3b3e09967730f7a9dc8e7b8edc02
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-13 05:05:40 +0700

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,9 @@
+// Description: This file contains all the routes for the tasks
 const express = require("express");
 
 const router = express.Router();
 
+// import controller
 const { getAllTasks } = require("../controllers/tasks");
 
 router.route("/").get(getAllTasks);
