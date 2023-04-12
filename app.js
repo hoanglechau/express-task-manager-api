a68ae0de0e0cc8aaaef37b94cb71282dd5eb6578
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-13 05:05:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Description: Entry point of the application
 const express = require("express");
 
+// create express app
 const app = express();
+
+// import routes
 const tasks = require("./routes/tasks");
 
 // middleware
@@ -11,8 +15,9 @@ app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
 
+// use routes
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-
+// start server
 app.listen(port, console.log(`Server is listening on port ${port}...`));
