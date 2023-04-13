bdb950c44f35f0f8da869d6e315d83cb710d8425
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-13 22:07:55 +0700

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,9 +3,17 @@ const express = require("express");
 
 const router = express.Router();
 
-// import controller
-const { getAllTasks } = require("../controllers/tasks");
+// import controllers
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
+} = require("../controllers/tasks");
 
-router.route("/").get(getAllTasks);
+// define routes
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
