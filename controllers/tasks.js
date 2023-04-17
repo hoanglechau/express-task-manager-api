13245324ac16c9f1728e7c51987f40c2111a6292
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-17 17:57:28 +0700

    Update the tasks controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,21 @@
 const Task = require("../models/Task");
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    // mongoose static function to get all tasks
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
