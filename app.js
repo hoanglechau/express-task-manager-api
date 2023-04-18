b4c8a943d275648fc1b50bc625b1d8d577227745
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-18 20:32:55 +0700

    Create middleware to handle page not found

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const express = require("express");
 
 // create express app
 const app = express();
-
 // import routes
 const tasks = require("./routes/tasks");
-
 // connect to database
 const connectDB = require("./db/connect");
+// import middleware to display 404 Page not found
+const notFound = require("./middleware/not-found");
+
 // middleware
 app.use(express.static("./public")); // to run the static frontend website
 app.use(express.json());
 
 // use routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
 const port = 3000;
 // start server
