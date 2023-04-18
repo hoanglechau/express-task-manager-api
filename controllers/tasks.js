b152bf85d23d6c56524294041d8ecc3607de4c54
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-18 20:32:19 +0700

    Add some more ways to show response status

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,6 +6,9 @@ const getAllTasks = async (req, res) => {
     // mongoose static function to get all tasks
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
+    // Some other ways to set response status:
+    //res.status(200).json({tasks, amount: tasks.length})
+    //res.status(200).json({status:'success',data:{tasks, nbHits:tasks.length}})
   } catch (error) {
     res.status(500).json({ msg: error });
   }
