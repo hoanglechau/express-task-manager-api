3d0d2683fbec4c6dc32be7dca5abb05d4215a73b
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-18 14:18:22 +0700

    Update the getTask controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,4 +1,6 @@
 const Task = require("../models/Task");
+
+// Get all tasks
 const getAllTasks = async (req, res) => {
   try {
     // mongoose static function to get all tasks
@@ -9,6 +11,7 @@ const getAllTasks = async (req, res) => {
   }
 };
 
+// Create a new task
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
@@ -18,8 +21,19 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+// Get an individual task with its id
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
